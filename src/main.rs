b7a5914889ca264fe3cffee85ec65300067a8c58
Dylan Socolobsky <dsocolobsky@gmail.com>
2021-06-16 21:01:12 -0300

    Load ROM into memory

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,27 @@ struct CPU {
     p: u8,   // Status register (lower 6 bits)
     pc: u16, // Program counter
     cycle: u32,
+    memory: Vec<u8>,
 }
 
 impl CPU {
 
     fn new() -> CPU {
-        CPU{a: 0, x: 0, y: 0, p: 0x34, s: 0xFD, pc: 0x00, cycle: 0}
+        CPU{a: 0, x: 0, y: 0, p: 0x34, s: 0xFD, pc: 0x10, cycle: 0, memory: Vec::new()}
     }
 
-    fn fetch() {
+    fn fetch(&mut self) {
+        println!("op: {:#02X?}", self.memory[self.pc as usize]);
+        self.pc += 1
+    }
 
+    fn load_rom(&mut self, filename: &str) {
+        self.memory = fs::read(Path::new(filename)).expect("Could not load ROM");
+        println!("{:#?}", self.memory.len())
+    }
+
+    fn log_memory(&self) {
+        println!("{:#02X?}", self.memory);
     }
 
     fn log_status(&self) {
@@ -32,18 +43,16 @@ impl CPU {
 fn main() -> Result<(), std::io::Error> {
     println!("nestum v0.01");
 
-    let path = Path::new("nestest/nestest.nes");
-    
-    /*let mut file = match File::open(&path) {
-        Err(why) => panic!("Could not open {}: {}", path.display(), why),
-        Ok(file) => file,
-    };*/
-
-    let content = fs::read(path)?;
-    println!("{:#02X?}", &content[16..64]);
+    //let content = fs::read(Path::new("nestest/nestest.nes"))?;
+    //println!("{:#02X?}", &content[16..64]);
 
     let mut cpu = CPU::new();
     cpu.log_status();
+    cpu.load_rom("nestest/nestest.nes");
+    cpu.fetch();
+    cpu.fetch();
+    cpu.fetch();
+    //cpu.log_memory();
 
     Ok(())
 }
